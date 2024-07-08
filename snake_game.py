5c2588998a96b7679a25f7aa915725c217e2c681
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-07-08 20:09:11 +0000

    only the agent can play

--- snake_game.py
+++ snake_game.py
@@ -157,18 +157,3 @@ class SnakeGame:
             
         self.head = Point(x, y)
             
-
-if __name__ == '__main__':
-    game = SnakeGame()
-    
-    # game loop
-    while True:
-        game_over, score = game.play_step()
-        
-        if game_over == True:
-            break
-        
-    print('Final Score', score)
-        
-        
-    pygame.quit()
